2af6ff8518bea35bd02b8c22237515b0c5ff485d
ail-ansari <84082786+ail-ansari@users.noreply.github.com>
2021-10-28 20:37:02 +0330

    Add files via upload

---
+++ 1/1-2/tamrin2.js
@@ -0,0 +1,80 @@
+let personData = [
+    {
+        uid: 1,
+        firstName: "Ali",
+        lastName: "Mahdavi"
+    },
+    {
+        uid: 2,
+        firstName: "Reza",
+        lastName: "Shahmardan"
+    },
+    {
+        uid: 3,
+        firstName: "Hassan",
+        lastName: "Qolami"
+    },
+    {
+        uid: 4,
+        firstName: "Morteza",
+        lastName: "Hamedani"
+    },
+    {
+        uid: 5,
+        firstName: "Sina",
+        lastName: "Hejazi"
+    },
+    {
+        uid: 6,
+        firstName: "Hadi",
+        lastName: "Sadri"
+    }
+]
+
+let additionalPersonData = [
+    {
+        uid: 3,
+        position: "UI Designer",
+        city: "Biarjmand"
+    },
+    {
+        uid: 1,
+        position: "Back-End Develope",
+        city: "Taleqan"
+    },
+    {
+        uid: 2,
+        position: "Front-End Developer",
+        city: "Behbahan"
+    },
+    {
+        uid: 4,
+        position: "Devops",
+        city: "Shiraz"
+    },
+    {
+        uid: 6,
+        position: "Server Admin",
+        city: "Tehran"
+    },
+    {
+        uid: 5,
+        position: "Product Manager",
+        city: "Hamedan"
+    }
+]
+let result = [
+    {
+
+    }
+]
+for (let i = 0; i < additionalPersonData.length; i++) {
+    let temp = {}
+    for (let j = 0; j < personData.length; j++) {
+        if (additionalPersonData[i].uid === personData[j].uid) {
+            temp = Object.assign(additionalPersonData[i], personData[j])
+            result.push(temp)
+        }
+    }
+}
+console.log(result);
